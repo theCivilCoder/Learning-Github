5fbcad3a1f277ef8aa503fd417ded9b88d845a66
Jimmy Zhu <yongjunjz@hotmail.com>
2022-05-20 20:35:50 -0600

    new comment added. should be pushed by theCivilCoder. not ENV

--- test_child.py
+++ test_child.py
@@ -3,3 +3,8 @@ print("inside child branch)
 
 #Comment added from MacbookPro
 # to test SSH key on computer with two SSH Keys
+## ^^ this new line was added by theCivilCoderENV
+
+
+# ANOTHER comment but this time user.email was set with the correct email
+# this commit should be added by personal 'theCivilCoder'
